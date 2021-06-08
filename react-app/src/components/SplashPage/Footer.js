6f92d0a537dc12148df2cacfed06eb02e4512454
Hector <crespomhector@gmail.com>
2021-06-08 13:01:50 -0400

    created navbar and body content for splash page

---
+++ react-app/src/components/SplashPage/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Footer;
--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import NavBar from './NavBar';
+import { NavLink } from 'react-router-dom';
+import Footer from "./Footer";
+
 import "./SplashPage.css"
+import backgroundImage from '../../images/backgroundImg.jpg';
 
 const SplashPage = () => {
     return (
-        <>
-            <NavBar/>
-            <h1>Welcome to the splash page!</h1>
-        </>
+        <div className="main_container">
+            <NavBar />
+            <div className="body_container">
+                <p className="header-1">
+                    Empower yourself by verbally affirming your goals and ambitions
+                </p>
+                <p className="header-2">
+                    Strengthen the connection between your unconscious mind and your consious mind
+                </p>
+                <p className="header-3">
+                    Challenge yourself to grow and become the person you envision
+                </p>
+                <p className="header-4">
+                    Choose from many daily quotes and create your own collection of favorite quotes
+                </p>
+                <NavLink className="sign_up" to="/sign-up">Create Account</NavLink>
+            </div>
+            <Footer />
+        </div>
     )
 }
 
