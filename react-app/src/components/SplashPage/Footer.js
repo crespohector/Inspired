1962780fc34a349288c416721e230773f3149c4e
Hector <crespomhector@gmail.com>
2021-06-08 14:13:06 -0400

    splash page finished

--- react-app/src/components/SplashPage/Footer.js
+++ react-app/src/components/SplashPage/Footer.js
@@ -4,9 +4,17 @@ import "./Footer.css";
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className="footer_container">
+            <div className="footer_container-content">
+                <span className="dev_name">@ Hector Crespo</span>
+                <a className="github_icon" href="https://github.com/crespohector/Inspired" >
+                    <i className="fab fa-github"></i>
+                </a>
+                <a className="linkedin_icon" href="http://linkedin.com/in/hector-crespo-b0b5b019a" >
+                    <i className="fab fa-linkedin"></i>
+                </a>
+            </div>
+        </footer>
     )
 }
 
