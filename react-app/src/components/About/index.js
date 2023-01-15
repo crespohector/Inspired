aaa1bad8b46428cfbcbaf46723e0db11719e6429
hector <crespomhector@gmail.com>
2023-01-15 13:15:26 -0500

    added media query and changes UI layout of the about page to utilize CSS grid. The aboutme page is now responsive to phone devices and tablets.

--- react-app/src/components/About/index.js
+++ react-app/src/components/About/index.js
@@ -6,27 +6,24 @@ const About = () => {
 
     return (
         <div className="about_main_wrapper">
-            <div className="about_me_container">
-                <div className="left_content">
-                    <div className="left_content-bio">
-                        <img src={bio} alt="bio" className="bio"/>
-                        <span className="first_name">Hector</span>
-                        <span className="last_name">Crespo</span>
-                        <p className="role">Full Stack Software Engineer</p>
-                    </div>
-                    <div className="left_content-links">
-                        <a className="left_content-github_icon" href="https://github.com/crespohector/Inspired" >
-                            <i className="fab fa-github"></i>
-                        </a>
-                        <a className="left_content-linkedin_icon" href="http://linkedin.com/in/hector-crespo-b0b5b019a" >
-                            <i className="fab fa-linkedin"></i>
-                        </a>
-                    </div>
+            <div className="aboutme-container">
+                <div className="profile">
+                    <img src={bio} alt="profile image" className="profile-img" />
+                    <span className="name">Hector Crespo</span>
+                    <p className="job-role">Full Stack Software Engineer</p>
                 </div>
-                <div className="right_content">
-                    <span className="right_content-hello">Hello</span>
-                    <p className="right_content-hello_sentence">Here's who I am and what I do</p>
-                    <p className="right_content-intro_one">I love the result of building full stack web applications from the ground-up. I am a problem-solver and enjoy debugging errors and figuring out solutions. I'm an enthusiastic teammate and have a passion for learning new technologies. I have experience building dynamic web applications in JavaScript, Python, React, Flask, SQLAlchemy, Express.js, Node.js, and PostgreSQL. When I am not coding, I take delight in eating delicious food, listening to podcasts, and excercising.</p>
+                <div className="about-links">
+                    <a className="github_icon" href="https://github.com/crespohector" target="_blank">
+                        <i className="fab fa-github"></i>
+                    </a>
+                    <a className="linkedin_icon" href="http://linkedin.com/in/hector-crespo-b0b5b019a" target="_blank">
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                </div>
+                <div className="intro-bio">
+                    <p className="hello">Hi there!</p>
+                    <p className="hello_sentence">Here's who I am and what I do</p>
+                    <p className="intro_one">I love the result of building full stack web applications from the ground-up. I am a problem-solver and enjoy debugging errors and figuring out solutions. I'm an enthusiastic teammate and have a passion for learning new technologies. I have experience building dynamic web applications in HTML5, CSS3, JavaScript, React.js, Python, Node.js, Express.js, Flask, SQLAlchemy, Alembic, Sequelize, PostgreSQL, SQLite3 and Playwright.</p>
                 </div>
             </div>
         </div>
