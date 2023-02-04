f947ea72272abe6c3f0de3f7ce73b0d9298ce8a9
hector <crespomhector@gmail.com>
2023-02-04 18:32:35 -0500

    fix 404 bug

--- app/__init__.py
+++ app/__init__.py
@@ -79,3 +79,7 @@ def react_root(path):
     if path == 'favicon.ico':
         return app.send_static_file('public', 'favicon.ico')
     return app.send_static_file('index.html')
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
